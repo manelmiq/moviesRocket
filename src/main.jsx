62ea4b2c2998405230fedec852e3a24c0863cefa
Emmanuell <emmanuellnogmiq@hotmail.com>
2024-06-27 16:14:06 -0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
-import { Routes } from './routes';
+import { Routes } from './routes/index.jsx';
 import { AuthProvider } from './hooks/auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
-import { useAuth } from '../hooks/auth';
+import {BrowserRouter} from "react-router-dom";
+import {useAuth} from '../hooks/auth';
 
-import { AppRoutes } from "./app.routes";
-import { AuthRoutes } from "./auth.routes";
+import {AppRoutes} from "./app.routes";
+import {AuthRoutes} from "./auth.routes";
 
-export function Routes() {
-  const { user } = useAuth();
+const Routes = () => {
+  const {user} = useAuth();
   
-  return(
+  return (
     <BrowserRouter>
-      { user ? <AppRoutes/> : <AuthRoutes/> }
+      {user ? <AppRoutes/> : <AuthRoutes/>}
     </BrowserRouter>
   )
 }
+export {Routes}
